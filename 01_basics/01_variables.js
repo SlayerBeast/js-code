2a50b26d9c4df3f2ebf20f395c60435dc61fca00
Rahul Kumar Pal <72176325+SlayerBeast@users.noreply.github.com>
2023-11-08 05:50:43 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14422
+let accountEmail = "rahul4kp"
+var accountPassword = "12345" //var is discountinued bcoz it can't handle scope 
+accountCity = "dhanbad"
+let accountState
+
+//console.log(accountEmail)
+
+console.table([accountId, accountEmail, accountState])
