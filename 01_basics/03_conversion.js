58d1e9f6227f0ba6b82deac21dce3c2d2624fd63
Rahul Kumar Pal <72176325+SlayerBeast@users.noreply.github.com>
2023-11-14 04:56:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,22 @@
+let score  = "33asd" //it will give NaN 
+let score1 = null
+
+//console.log(typeof(score));
+//console.log(typeof(score1));
+let value = Number(score)
+
+//console.log(value);
+//console.log(score1);
+
+// "33" => 33
+// "33asd" => NaN
+// true => 1
+
+let isLoggedIn =1
+
+let booleanIsloggedin = Boolean(isLoggedIn)
+console.log(booleanIsloggedin);
+
+// 1 => true 0 => false
+// "" => false
+// "rahul" => true
